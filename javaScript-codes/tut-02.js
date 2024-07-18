61cf8e693c03c4cc061bff37738b013023a33fbe
Lizpy <yify.fury@gmail.com>
2024-07-18 07:06:14 +0530

    variable declaration

--- javaScript-codes/tut-02.js
+++ javaScript-codes/tut-02.js
@@ -1,15 +1,30 @@
-//creating an object in javaScript
-let rectangle={
-   length: 1,
-   breadth:2,
-   draw(){
-    console.log("drawing a rectangle");
-   },
-   delete(){
-    console.log("delete the rect");
-   }
-};  
-console.log("length: "+rectangle.length+" breadth: "+rectangle.breadth);
-//calling a function
-rectangle.draw();
-rectangle.delete();  
+ //using let,var,const for declaring variable
+// const accountId=12345;
+// let accountEmail="vk@gmail.com"
+// var accountPass="s%5B5S5twfV^K"
+// accountCity="Bangalore"
+// console.table([accountId,accountEmail,accountPass,accountCity]);
+
+// ----scope of let,var
+if(true)
+{
+  let a=5;
+  var b=5;  
+}
+console.log(++a);//throw  an error since scope of let is under the braces only
+console.log(++b);
+// //creating an object in javaScript
+// let rectangle={
+//    length: 1,
+//    breadth:2,
+//    draw(){
+//     console.log("drawing a rectangle");
+//    },
+//    delete(){
+//     console.log("delete the rect");
+//    }
+// };  
+// console.log("length: "+rectangle.length+" breadth: "+rectangle.breadth);
+// //calling a function
+// rectangle.draw();
+// rectangle.delete(); 
