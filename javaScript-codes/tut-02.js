28da42bef21a2198798dd2c8037d8b4626719d72
Lizpy <yify.fury@gmail.com>
2024-07-19 20:46:30 +0530

    final commit for tut-02

--- javaScript-codes/tut-02.js
+++ javaScript-codes/tut-02.js
@@ -1,18 +1,21 @@
- //using let,var,const for declaring variable
-// const accountId=12345;
-// let accountEmail="vk@gmail.com"
-// var accountPass="s%5B5S5twfV^K"
-// accountCity="Bangalore"
-// console.table([accountId,accountEmail,accountPass,accountCity]);
-
+//  using let,var,const for declaring variable
+const accountId=12345;
+let accountEmail="vk@gmail.com"
+var accountPass="s%5B5S5twfV^K"
+accountCity="Bangalore"
+let getPass="false"
+//array declaration
+let userInfo=[accountId,accountEmail,accountPass,accountCity,getPass];
+console.table([accountId,accountEmail,accountPass,accountCity]);
+console.log(userInfo[0]+userInfo[1]+userInfo[3]);
 // ----scope of let,var
-if(true)
-{
-  let a=5;
-  var b=5;  
-}
-console.log(++a);//throw  an error since scope of let is under the braces only
-console.log(++b);
+// if(true)
+// {
+//   let a=5;
+//   var b=5;  
+// }
+// console.log(++a);//throw  an error since scope of let is under the braces only
+// console.log(++b);
 // //creating an object in javaScript
 // let rectangle={
 //    length: 1,
@@ -28,3 +31,4 @@ console.log(++b);
 // //calling a function
 // rectangle.draw();
 // rectangle.delete(); 
+
